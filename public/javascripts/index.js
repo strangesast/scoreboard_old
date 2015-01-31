20779d2f7cc99adb0ac03bb6239c5091961c697c
Sam Zagrobelny <strangesast@gmail.com>
2015-01-31 11:27:23 -0500

    building scoreboard animation

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,234 +1,108 @@
-$(document).ready(function() {
-});
+var canvas = document.getElementById('logo');
 
-$('button').on('click', function() {
-  console.log('toast');
-	test();
-});
+canvas.width = 400;
+canvas.height =  200;
 
-var obj = {'_id':'01kdka93d'};
+var boards = [];
 
-function test() {
-  var promise = $.ajax({
-		url: '/api/abc/ad/da/',
-		contentType:"application/json",
-		type: 'PUT',
-		data: JSON.stringify(obj)
-	});
-
-	promise.done(function(err, data) {
-		console.log(err);
-		console.log(data);
-	});
+function Board(parentCanvas, x, y, number, color) {
+	console.log('new board');
+	var boardWidth = 40;
+	var boardHeight = 40;
+	this.largeRoundRadius = 6;
+	this.smallRoundRadius = 2;
+	this.parent = parentCanvas;
+	this.ctx= parentCanvas.getContext('2d');
+	this.x = x;
+	this.y = y;
+	this.w = boardWidth;
+	this.h = boardHeight;
+	this.number = number || 0;
+	this.color = color || "blue";
 }
 
-//$(document).ready(function() {
-//	console.log('toast Ready toast');
-//	$('.thumbnail').css("background-color", "yellow");
-//	$('#datetimepicker1').datetimepicker();
-//});
-//
-//var validMethods = ['get', 'add'];
-//var localUrl = '/api';
-//
-//function getName() {
-//	return document.getElementById('nameInput').value;
-//}
-//
-//function getDesc() {
-//	return document.getElementById('descInput').value;
-//}
-//
-//function getType() {
-//	return document.getElementById('typeInput').value;
-//}
-//
-//function getTeam() {
-//	var input = document.getElementById('teamInput');
-//	var value = input.options[input.selectedIndex].value;
-//	return value;
-//}
-//
-//function getAddTeam() {
-//	var input = document.getElementById('addTeamInput');
-//	var inputOption = input.options[input.selectedIndex];
-//	if(inputOption !== undefined) {
-//		var value = inputOption.value;
-//	} else {
-//		var value = undefined;
-//	}
-//	return value;
-//}
-//
-//function getGly(index, css) {
-//	return (css.match (/(^|\s)glyphicon-\S+/g) || []).join(' ');
-//}
-//
-//// control which rows are visible for each type
-//$('#typeInput').on('input', function(eventObj) {
-//  $('#objectName').text('Example Name');
-//	$('#objectDesc').text('Lorum ipsum dolorem...');
-//	$('input[type=text]').each(function() {this.value = ""});
-//	if(this.value == 'event') {
-//		$('#compRow').addClass('hidden');
-//		$('#nameRow').removeClass('hidden');
-//    $('#descRow').removeClass('hidden');
-//		$('.thumbnail').css("background-color", "lightgreen");
-//		$('#timeRow').removeClass('hidden');
-//		$('#gameRow').removeClass('hidden');
-//		$('#teamRow').addClass('hidden');
-//		$('#prevIcon').removeClass(getGly).addClass('glyphicon-tasks');
-//	} else if(this.value == 'player') {
-//		$('#compRow').addClass('hidden');
-//		$('#nameRow').removeClass('hidden');
-//    $('#descRow').addClass('hidden');
-//		$('.thumbnail').css("background-color", "lightblue");
-//		$('#teamRow').removeClass('hidden');
-//		$('#timeRow').addClass('hidden');
-//		$('#gameRow').addClass('hidden');
-//		$('#prevIcon').removeClass(getGly).addClass('glyphicon-user');
-//		$('#objectDesc').text(getTeam());
-//	} else if(this.value == 'game') {
-//		$('#compRow').removeClass('hidden');
-//		$('#nameRow').removeClass('hidden');
-//		$('.thumbnail').css("background-color", "yellow");
-//    $('#descRow').addClass('hidden');
-//		$('#timeRow').addClass('hidden');
-//		$('#teamRow').addClass('hidden');
-//		$('#gameRow').addClass('hidden');
-//		$('#prevIcon').removeClass(getGly).addClass('glyphicon-bullhorn');
-//	} else if(this.value == 'team') {
-//		$('#compRow').addClass('hidden');
-//		$('#nameRow').removeClass('hidden');
-//		$('.thumbnail').css("background-color", "purple");
-//		$('#timeRow').addClass('hidden');
-//		$('#teamRow').addClass('hidden');
-//		$('#gameRow').addClass('hidden');
-//		$('#prevIcon').removeClass(getGly).addClass('glyphicon-link');
-//	}
-//});
-//
-//
-//$('#teamInput').on('input', function() {
-//  if(['player'].indexOf(getType() > -1)) {
-//		$('#objectDesc').text(getTeam());
-//	}
-//});
-//
-//
-//$('#nameInput').on('input', function() {
-//	$('#objectName').text(this.value);
-//});
-//
-//
-//$('#descInput').on('input', function() {
-//	if(['event', 'team'].indexOf(getType()) > -1) {
-//    $('#objectDesc').text(this.value);
-//	}
-//});
-//
-//
-//$('#addTeam').on('click', function() {
-//	var source = $('#testTemplate').html();
-//	var template = Handlebars.compile(source);
-//	var value = getAddTeam();
-//	if(value) {
-//	  var data = {'name' : value};
-//	  $('#compRow > .form-group').append(template(data));
-//	  $('#addTeamInput > option[value=' + value + ']').prop('disabled', true);
-//	  $('#addTeamInput').val('0');
-//	  refreshDescription();
-//	}
-//});
-//
-//function removeGame(obj) {
-//	var _parent = $(obj).parent();
-//	var value = _parent.children(":first").text();
-//	$('#addTeamInput > option[value=' + value + ']').prop('disabled', false);
-//	_parent.remove();
-//	$('#addTeamInput').val('0');
-//	refreshDescription();
-//}
-//
-//// return teams that have been chosen
-//function getSelectedTeams() {
-//	var teams = [];
-//	var teamList = $('.team-box');
-//  for(var i=0; i<teamList.length; i++) {
-//		var team = $(teamList[i]).children(':first').text();
-//		teams.push(team);
-//	}
-//	return teams;
-//}
-//
-//// refresh preview description
-//function refreshDescription() {
-//	document.getElementById('objectDesc').innerHTML = "";
-//	var teams = getSelectedTeams();
-//	console.log(teams);
-//	var index = teams.indexOf('unattached');
-//	if(index > -1) {
-//		teams.splice(index, 1);
-//	}
-//	if(teams.length > 4) {
-//		teams = teams.slice(0, 4);
-//	  var text = teams.join([separator=' vs '])
-//	  text = text + '...';
-//	} else {
-//	  var text = teams.join([separator=' vs '])
-//	}
-//	document.getElementById('objectDesc').innerHTML = text;
-//}
-//
-//$('#submit').on('click', function(eventObj) {
-//	var obj = {'method':'add'};
-//	var what = {};
-//	what.type = getType();
-//
-//	if(what.type == 'event') {
-//    var date = $('#datetimepicker1').data('DateTimePicker').getDate();
-//		what.date = date;
-//		what.parent = document.getElementById('gameInput').value;
-//		what.description = document.getElementById('objectDesc').innerHTML;
-//
-//	} else if(what.type == 'game') {
-//		what.name = getName();
-//		what.teams = getSelectedTeams();
-//
-//	} else if(what.type == 'player') {
-//		what.team = document.getElementById('teamInput').value;
-//	}
-//	var name = document.getElementById('nameInput').value
-//	what.name = name;
-//  obj.what = [what];
-//
-//	$('input, select').prop('disabled', true);
-//	ajaxy(localUrl, JSON.stringify(obj), finishAddition);
-//});
-//
-//function finishAddition(_data) {
-//	if(_data != 'invalid' && _data != 'timeout') {
-//	  $('.littleContainer').children().not('.preview').animate({"opacity":"0"}, 500);
-//		$('.littleContainer').animate({"height": "200px"}, 500);
-//	  $('.preview').animate({"top": "25%", "left":"25%", "width": "300px"}, 500);
-//		setTimeout(function () {
-//			window.location.href = "/players";
-//		}, 700);
-//	} else {
-//	  $('input, select').prop('disabled', false);
-//		// trigger that it was bad
-//	}
-//}
-//
-//function ajaxy(url, obj, callback) {
-//  var promise = $.ajax({
-//		url: url,
-//		contentType:"application/json", // this is essential
-//		type: 'POST',
-//		data: obj
-//	});
-//
-//	promise.done(function(data, err) {
-//		finishAddition(data);
-//	});
-//}
+Board.prototype.animate = function(step) {
+	var ctx = this.ctx;
+	var x = this.x;
+	var y = this.y;
+	var w = this.w;
+	var h = this.h;
+	var lrr = this.largeRoundRadius;
+	var srr = this.smallRoundRadius;
+
+	function pro(delay) {
+		return new Promise(function(resolve, reject) {
+			setTimeout(function() {
+				resolve(true);
+			}, delay);
+		});
+	}
+
+	ctx.translate(x, y);
+	for(var i=0; i<0.5; i+=0.1) {
+		var p = new Promise(function(resolve, reject) {
+			console.log('frame');
+		  ctx.save();
+	    ctx.transform(1, 0, i, 1-i, 0, 0);
+      ctx.beginPath();
+	    ctx.moveTo(lrr, 0);
+	    ctx.arc(lrr, lrr, lrr, 3*Math.PI/2, Math.PI, true);
+	    ctx.lineTo(0, h-lrr);
+	    ctx.arc(srr, h - srr, srr, Math.PI, Math.PI/2, true);
+	    ctx.lineTo(w-srr, h);
+	    ctx.arc(w - srr, h - srr, srr, Math.PI/2, 0, true);
+	    ctx.lineTo(w, lrr);
+	    ctx.arc(w - lrr, lrr, lrr, 0, -Math.PI/2, true);
+	    ctx.lineTo(lrr, 0);
+	    ctx.lineWidth = 2;
+	    ctx.stroke();
+	    ctx.fillStyle = "blue";
+	    ctx.fill();
+	    ctx.closePath();
+	    ctx.beginPath();
+	    ctx.fillStyle = "white";
+	    ctx.arc(w/3, lrr, srr, 0, 2*Math.PI);
+	    ctx.arc(w*2/3, lrr, srr, 0, 2*Math.PI);
+	    ctx.fill();
+		  ctx.font = "35px Verdana";
+		  ctx.fillText("9", w/4, 35);
+		  ctx.restore();
+		});
+
+		Promise.all([p, pro(10000*i)]).then(function(values) {console.log(values)});
+	}
+
+	//for(var i=0.5; i<1.1; i+=0.1) {
+	//	setTimeout(function() {
+	//	//ctx.fillStyle = "white";
+	//	//ctx.fillRect(0, 0, 200, 200);
+	//  //ctx.transform(1, 0, 0.75, 1, 0, 0);
+	//  ctx.beginPath();
+	//  ctx.moveTo(x + lrr, y);
+	//  ctx.arc(x + lrr, y + lrr, lrr, 3*Math.PI/2, Math.PI, true);
+	//  ctx.lineTo(x, y+this.h-lrr);
+	//  ctx.arc(x + srr, y + this.h - srr, srr, Math.PI, Math.PI/2, true);
+	//  ctx.lineTo(x+this.w-srr, y + this.h);
+	//  ctx.arc(x + this.w - srr, y + this.h - srr, srr, Math.PI/2, 0, true);
+	//  ctx.lineTo(x + this.w, y + lrr);
+	//  ctx.arc(x + this.w - lrr, y + lrr, lrr, 0, -Math.PI/2, true);
+	//  ctx.lineTo(x + lrr, y);
+	//  ctx.lineWidth = 2;
+	//  ctx.stroke();
+	//  ctx.fillStyle = "blue";
+	//  ctx.fill();
+	//  ctx.closePath();
+	//  ctx.beginPath();
+	//  ctx.fillStyle = "white";
+	//  ctx.arc(x+this.w/3, y+this.h-lrr, srr, 0, 2*Math.PI);
+	//  ctx.arc(x+this.w*2/3, y+this.h-lrr, srr, 0, 2*Math.PI);
+	//  ctx.fill();
+	//	}, i*1000);
+	//}
+
+}
+
+
+var b = new Board(canvas, 20, 20, 1, "blue");
+
+b.animate();
