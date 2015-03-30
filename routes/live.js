1a67ffa5ea1f900b770f3d5b5536698507d1f0b7
Sam Zagrobelny <strangesast@gmail.com>
2015-03-30 12:01:23 -0400

    added live

--- routes/live.js
+++ routes/live.js
@@ -7,10 +7,20 @@ var config = require('../config');
 var mongoUrl = config.mongoUrl;
 var url = require('url');
 var Promise = require('es6-promise').Promise;
+var WebSocketServer = require('ws').Server,
+	  wss = new WebSocketServer({port : 3030});
 
+console.log('toast');
 // "event" list is stored for an entire region.  adding to message queue
 // notified clients and client-servers of change as well as adding to actual
 // relevant objects in /api/
+
+// example "event"
+// {'parent_type' : 'game',
+//  'action'      : 'score_change',
+//  'value'       : '+1',
+//  'parent_id'   : 'abadcdadkfjak'}
+
 
 router.post('*', function(req, res, next) {
 	// general verifications
@@ -22,7 +32,6 @@ router.route('/')
   // list all available "live" objects, probably
 	res.json('ok');
 })
-
 
 router.route('/region/:regionIdentifier?')
 .get(function(req, res, next) {
@@ -36,17 +45,23 @@ router.route('/region/:regionIdentifier?')
 	  res.json(regionIdentifier);
 	}
 })
-// require that a region is provided
+// require that a region is provided if not simply listing them
 .all(function(req, res, next) {
   var body = req.body;
 	if(!('region' in body) || !ObjectID.isValid(body.region)) {
-		next(new Error('you done fucked up'));
+		next(new Error('must specify region to add to'));
 	} else return next();
 })
 .post(function(req, res, next) {
 // add to live stack, send corresponding messages
+// check that region and corresponding live collection exists for region 
 	res.json(req.params.regionIdentifier);
 });
 
 
+
+// websocket server (probably a bad place for this)
+wss.on('connection', function(ws) {
+
+})
 module.exports = router;
