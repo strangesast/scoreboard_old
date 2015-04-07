c4eaf2b7c95708c87424aaef78be40ba94fe0be3
Sam Zagrobelny <strangesast@gmail.com>
2015-04-07 15:42:36 -0400

    messaging works, for real, output to console

--- routes/live.js
+++ routes/live.js
@@ -8,6 +8,8 @@ var Promise = require('es6-promise').Promise;
 var WebSocketServer = require('ws').Server,
 	  wss = new WebSocketServer({port : 3030});
 var models = require('../models');
+var net = require('net');
+var JsonSocket = require('json-socket');
 
 var default_limit = 50;
 
@@ -50,6 +52,28 @@ router.get('/', function(req, res, next) {
 var tcp_listeners = {};
 var ws_listeners = {};
 
+// tcp
+var port = 8084;
+var server = net.createServer();
+server.on('connection', function(socket) {
+  console.log('connected')
+  socket = new JsonSocket(socket);
+  socket.on('message', function(message) {
+    console.log(message);
+  });
+
+  models.events.on('action', function(action) {
+  	socket.sendMessage(JSON.stringify(action));
+  
+    // if action matches any of the listeners, "join" it and send
+  })
+  
+  socket.sendMessage('connected to scoreboard');
+
+}).listen(port, function() {
+  console.log('listening on ' + port + ' for tcp connections');
+});
+
 // websocket server (probably a bad place for this)
 wss.on('connection', function(ws) {
 	ws.on('message', function(message) {
@@ -60,11 +84,6 @@ wss.on('connection', function(ws) {
 })
 
 // listen for new actions
-models.events.on('action', function(action) {
-	console.log(action);
-  // if action matches any of the listeners, "join" it and send
-})
-
 
 
 module.exports = router;
